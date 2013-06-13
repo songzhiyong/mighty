340b9883031a6ff74e3ff48e7a3400a98ed6d68f
jerome <songzhiyong1121@gmail.com>
2013-06-13 16:47:53 +0800

    tabs

---
+++ src/com/jerome/mighty/app/TabsActivity.java
@@ -0,0 +1,38 @@
+/**
+ * TabsActivity.java
+ * com.jerome.mighty.app
+ *
+ * Function： TODO 
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2013-6-13 		Jerry
+ *
+ * Copyright (c) 2013, JEROME All Rights Reserved.
+ */
+
+package com.jerome.mighty.app;
+
+import android.os.Bundle;
+
+import com.jerome.base.BaseActivity;
+import com.jerome.mighty.R;
+
+/**
+ * ClassName:TabsActivity Function: TODO ADD FUNCTION Reason: TODO ADD REASON
+ * 
+ * @author Jerry
+ * @version
+ * @since Ver 1.1
+ * @Date 2013-6-13 下午4:08:45
+ * 
+ * @see
+ */
+public class TabsActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_tabs);
+	}
+
+}

e9aeddbd720d056640b3761fa7d26cc0f29ea9f9
jerome <songzhiyong1121@gmail.com>
2013-06-13 16:04:12 +0800

    jazz

--- src/com/jerome/mighty/app/MainActivity.java
+++ src/com/jerome/mighty/app/MainActivity.java
@@ -2,19 +2,26 @@ package com.jerome.mighty.app;
 
 import java.util.ArrayList;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
-import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
 
-import com.jerome.base.Log;
 import com.jerome.mighty.R;
 import com.jerome.mighty.temp.ColorFragment;
 import com.jerome.mighty.temp.SampleListFragment;
+import com.jfeinstein.jazzyviewpager.JazzyViewPager;
+import com.jfeinstein.jazzyviewpager.JazzyViewPager.TransitionEffect;
+import com.jfeinstein.jazzyviewpager.OutlineContainer;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.viewpagerindicator.TabPageIndicator;
@@ -33,9 +40,10 @@ import com.viewpagerindicator.TabPageIndicator;
  * @see
  */
 public class MainActivity extends SlidingFragmentActivity {
-	private static final String[] CONTENT = new String[] { "1", "2",
-			"3", "4", "5", "6" };
+	private static final String[] CONTENT = new String[] { "1", "2", "3", "4",
+			"5", "6" };
 	protected ListFragment mFrag;
+	private JazzyViewPager vp;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -46,48 +54,67 @@ public class MainActivity extends SlidingFragmentActivity {
 		mFrag = new SampleListFragment();
 		t.replace(R.id.menu_frame, mFrag);
 		t.commit();
-		// customize the SlidingMenu
+		// customize the SlidingMenu////////////////////
 		SlidingMenu sm = getSlidingMenu();
 		sm.setShadowWidthRes(R.dimen.shadow_width);
 		sm.setShadowDrawable(R.drawable.shadow);
 		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
 		sm.setFadeDegree(0.35f);
 		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		sm.setMode(SlidingMenu.LEFT_RIGHT);
+		sm.setSecondaryMenu(R.layout.menu_frame_two);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.menu_frame_two, new SampleListFragment())
+				.commit();
+		sm.setShadowDrawable(R.drawable.shadow);
+
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		ViewPager vp = (ViewPager) findViewById(R.id.pager);
+		// viewpager/////////////////////////////////
+		vp = (JazzyViewPager) findViewById(R.id.jazzy_pager);
+		vp.setTransitionEffect(TransitionEffect.CubeOut);
 		vp.setAdapter(new ColorPagerAdapter(getSupportFragmentManager()));
+		// vp.setAdapter(new MainAdapter());
+		vp.setPageMargin(30);
 		vp.setCurrentItem(0);
 		setBehindContentView(R.layout.menu_frame);
 		TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
 		indicator.setViewPager(vp);
-		indicator.setOnPageChangeListener(new OnPageChangeListener() {
-			public void onPageScrollStateChanged(int arg0) {
-			}
-
-			public void onPageScrolled(int arg0, float arg1, int arg2) {
-			}
-
-			public void onPageSelected(int position) {
-				switch (position) {
-				case 0:
-					Log.i("MainActivity", "TOUCHMODE_FULLSCREEN");
-					getSlidingMenu().setTouchModeAbove(
-							SlidingMenu.TOUCHMODE_FULLSCREEN);
-					break;
-				default:
-					Log.i("MainActivity", "TOUCHMODE_MARGIN");
-					getSlidingMenu().setTouchModeAbove(
-							SlidingMenu.TOUCHMODE_MARGIN);
-					break;
-				}
-			}
-		});
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+//		indicator.setOnPageChangeListener(new OnPageChangeListener() {
+//			public void onPageScrollStateChanged(int arg0) {
+//			}
+//
+//			public void onPageScrolled(int arg0, float arg1, int arg2) {
+//			}
+//
+//			public void onPageSelected(int position) {
+//				switch (position) {
+//				case 0:
+//					Log.i("MainActivity", "TOUCHMODE_FULLSCREEN");
+//					getSlidingMenu().setTouchModeAbove(
+//							SlidingMenu.TOUCHMODE_FULLSCREEN);
+//					break;
+//				default:
+//					Log.i("MainActivity", "TOUCHMODE_MARGIN");
+//					getSlidingMenu().setTouchModeAbove(
+//							SlidingMenu.TOUCHMODE_MARGIN);
+//					break;
+//				}
+//			}
+//		});
 	}
 
 	public class ColorPagerAdapter extends FragmentPagerAdapter {
 		private ArrayList<Fragment> mFragments;
 		private final int[] COLORS = new int[] { R.color.red, R.color.green,
-				R.color.blue, R.color.white, R.color.black };
+				R.color.blue, R.color.holo_blue, R.color.black };
+
+		@Override
+		public Object instantiateItem(ViewGroup container, final int position) {
+			Object obj = super.instantiateItem(container, position);
+			vp.setObjectForPosition(obj, position);
+			return obj;
+		}
 
 		public ColorPagerAdapter(FragmentManager fm) {
 			super(fm);
@@ -107,6 +134,45 @@ public class MainActivity extends SlidingFragmentActivity {
 
 		public Fragment getItem(int position) {
 			return mFragments.get(position);
+		}
+	}
+
+	private class MainAdapter extends PagerAdapter {
+		@Override
+		public Object instantiateItem(ViewGroup container, final int position) {
+			TextView text = new TextView(MainActivity.this);
+			text.setGravity(Gravity.CENTER);
+			text.setTextSize(30);
+			text.setTextColor(Color.WHITE);
+			text.setText("Page " + position);
+			text.setPadding(30, 30, 30, 30);
+			int bg = Color.rgb((int) Math.floor(Math.random() * 128) + 64,
+					(int) Math.floor(Math.random() * 128) + 64,
+					(int) Math.floor(Math.random() * 128) + 64);
+			text.setBackgroundColor(bg);
+			container.addView(text, LayoutParams.MATCH_PARENT,
+					LayoutParams.MATCH_PARENT);
+			vp.setObjectForPosition(text, position);
+			return text;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object obj) {
+			container.removeView(vp.findViewFromObject(position));
+		}
+
+		@Override
+		public int getCount() {
+			return 10;
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object obj) {
+			if (view instanceof OutlineContainer) {
+				return ((OutlineContainer) view).getChildAt(0) == obj;
+			} else {
+				return view == obj;
+			}
 		}
 	}
 

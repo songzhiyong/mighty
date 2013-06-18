eeeea661ae0a559dc10a07c7046c42977c53e732
jerome <songzhiyong1121@gmail.com>
2013-06-18 17:07:12 +0800

    lo

--- src/com/jerome/mighty/app/LoginActivity.java
+++ src/com/jerome/mighty/app/LoginActivity.java
@@ -13,9 +13,13 @@
 
 package com.jerome.mighty.app;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 import com.jerome.base.BaseActivity;
+import com.jerome.mighty.R;
 
 /**
  * ClassName:LoginActivity<br>
@@ -31,5 +35,12 @@ public class LoginActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.layout_login);
+		ImageView img = (ImageView) findViewById(R.id.iv_login_main);
+		img.setBackgroundResource(R.drawable.login_main);
+		AnimationDrawable frameAnimation = (AnimationDrawable) img
+				.getBackground();
+		frameAnimation.start();
 	}
 }
